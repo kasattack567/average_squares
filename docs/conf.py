53affd0ca7a5f0587201113bb55e59bc20c2c3ea
Qasim Bedford <zcapqbe@ucl.ac.uk>
2025-11-10 01:15:43 +0000

    generation documentation with Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+import os
+import sys
+# Add the parent directory (one level up) to the system path
+# so Sphinx can find your Python modules such as squares.py
+sys.path.insert(0, os.path.abspath('..'))
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Qasim_Project'
+copyright = '2025, Qasim Bedford'
+author = 'Qasim Bedford'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',      # Enables autodoc to pull docstrings from code
+    'sphinx.ext.autosummary',  # Optional: auto-generates summaries for modules
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,22 @@
+.. Qasim_Project documentation master file, created by
+   sphinx-quickstart on Mon Nov 10 00:40:23 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Qasim_Project's documentation!
+=========================================
+This is a placeholder for the documentation of the Qasim_Project. Here you will find all the necessary information to get started and make the most out of this project.
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   content/average-squares-docs
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
